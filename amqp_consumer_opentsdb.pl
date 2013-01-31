73671676a9ce75ea5080adf0a6adcf3209a4c90e
Mark Steele <mark@control-alt-del.org>
2013-01-31 16:24:58 -0500

    re-using socket

--- amqp_consumer_opentsdb.pl
+++ amqp_consumer_opentsdb.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+## TODO: error checking around dead socket....
+
 use strict;
 use Data::Dumper;
 use Net::RabbitMQ;
@@ -26,6 +28,10 @@ if ($options{'compress'}) {
   use Compress::Snappy;
 }
 
+my $sock = IO::Socket::INET->new(PeerAddr => $options{'opentsdb_host'}, PeerPort => $options{'opentsdb_port'}, Proto => 'tcp', Timeout  => 1);
+$sock->sockopt(SO_KEEPALIVE);
+die("Unable to connect") unless $sock->connected();
+
 my $mq = Net::RabbitMQ->new();
 $mq->connect($options{'amqp_host'} , { port => $options{'amqp_port'}, user => $options{'amqp_user'}, password => $options{'amqp_password'}, vhost => $options{'amqp_vhost'} });
 $mq->channel_open(1);
@@ -33,42 +39,33 @@ $mq->queue_declare(1, $options{'amqp_queue'});
 $mq->queue_bind(1, $options{'amqp_queue'}, $options{'amqp_exchange'}, '#');
 $mq->consume(1,$options{'amqp_queue'});
 
-### Assuming metric format is 'some.metric.name.datacenter.host', tagged in opentsdb with datacenter= and host=
-
 while(1) {
   my $msg = $mq->recv();
   if ($msg) {
     if ($options{'compress'}) {
       $msg->{'body'} = decompress($msg->{'body'});
     }
-    my $sock = IO::Socket::INET->new(PeerAddr => $options{'opentsdb_host'},
-                                      PeerPort => $options{'opentsdb_port'},
-                                      Proto    => 'tcp',
-                                      Timeout  => 1);
-    if ($sock) {
-      foreach my $line (split(/\n/, $msg->{'body'})) {
-        my $metric = {};
-        eval {
-          $metric = decode_json($line);
-        };
-        if ($@) {
-          print "Error parsing input data: $line\n";
-          next;
-        }
-        my $output = sprintf("put %s %d %s", $metric->{'plugin'},$metric->{'time'},$metric->{'value'});
-        delete($metric->{'plugin'});
-        delete($metric->{'time'});
-        delete($metric->{'value'});
-        foreach my $k (keys %{$metric}) {
-          my $tk = $k;
-          $tk =~ s/[ =]//g;
-          $metric->{$k} =~ s/[ =]//g;
-          $output .= ' ' . $tk . '=' . $metric->{$k};
-        } 
-        $output .= "\n";
-	print $sock $output;
+    foreach my $line (split(/\n/, $msg->{'body'})) {
+      my $metric = {};
+      eval {
+        $metric = decode_json($line);
+      };
+      if ($@) {
+        print "Error parsing input data: $line\n";
+        next;
       }
-      close($sock);
+      my $output = sprintf("put %s %d %s", $metric->{'plugin'},$metric->{'time'},$metric->{'value'});
+      delete($metric->{'plugin'});
+      delete($metric->{'time'});
+      delete($metric->{'value'});
+      foreach my $k (keys %{$metric}) {
+        my $tk = $k;
+        $tk =~ s/[ =]//g;
+        $metric->{$k} =~ s/[ =]//g;
+        $output .= ' ' . $tk . '=' . $metric->{$k};
+      } 
+      $output .= "\n";
+      print $sock $output;
     }
   } else {
     sleep(1);
